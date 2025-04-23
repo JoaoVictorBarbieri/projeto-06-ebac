33131a3ca1e2cecf045270d8f25032c02e623d8d
Joaquinn <joaov.b.euwest@gmail.com>
2025-04-23 17:13:32 -0300

    #Perfil

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,4 +1,5 @@
 import logo from '../../assets/images/logo.png'
+import imagemPerfil from '../../assets/images/pizza.png'
 import Footer from '../Footer'
 import {
   Imagem,
@@ -9,8 +10,14 @@ import {
   Categoria,
   NomeRestaurante,
   ContentWrapper,
-  ContainerSmaller
+  ContainerSmaller,
+  ProdutoSection,
+  Produto,
+  ProdutoUl,
+  ImagemPefil,
+  ProdutoBox
 } from './style'
+import { Botao } from '../List/style'
 const Header = () => (
   <>
     <Container>
@@ -32,6 +39,82 @@ const Header = () => (
         <NomeRestaurante>La Dolce Vita Trattoria</NomeRestaurante>
       </ContentWrapper>
     </SubHeader>
+    <ProdutoSection>
+      <ProdutoUl>
+        <Produto>
+          <ProdutoBox>
+            <ImagemPefil src={imagemPerfil} alt="Pizza" />
+            <h3>Pizza Marguerita</h3>
+            <p>
+              A clássica Marguerita: molho de tomate suculento, mussarela
+              derretida, manjericão fresco e um toque de azeite. Sabor e
+              simplicidade!
+            </p>
+            <Botao>Adicionar ao carrinho</Botao>
+          </ProdutoBox>
+        </Produto>
+        <Produto>
+          <ProdutoBox>
+            <ImagemPefil src={imagemPerfil} alt="Pizza" />
+            <h3>Pizza Marguerita</h3>
+            <p>
+              A clássica Marguerita: molho de tomate suculento, mussarela
+              derretida, manjericão fresco e um toque de azeite. Sabor e
+              simplicidade!
+            </p>
+            <Botao>Adicionar ao carrinho</Botao>
+          </ProdutoBox>
+        </Produto>
+        <Produto>
+          <ProdutoBox>
+            <ImagemPefil src={imagemPerfil} alt="Pizza" />
+            <h3>Pizza Marguerita</h3>
+            <p>
+              A clássica Marguerita: molho de tomate suculento, mussarela
+              derretida, manjericão fresco e um toque de azeite. Sabor e
+              simplicidade!
+            </p>
+            <Botao>Adicionar ao carrinho</Botao>
+          </ProdutoBox>
+        </Produto>
+        <Produto>
+          <ProdutoBox>
+            <ImagemPefil src={imagemPerfil} alt="Pizza" />
+            <h3>Pizza Marguerita</h3>
+            <p>
+              A clássica Marguerita: molho de tomate suculento, mussarela
+              derretida, manjericão fresco e um toque de azeite. Sabor e
+              simplicidade!
+            </p>
+            <Botao>Adicionar ao carrinho</Botao>
+          </ProdutoBox>
+        </Produto>
+        <Produto>
+          <ProdutoBox>
+            <ImagemPefil src={imagemPerfil} alt="Pizza" />
+            <h3>Pizza Marguerita</h3>
+            <p>
+              A clássica Marguerita: molho de tomate suculento, mussarela
+              derretida, manjericão fresco e um toque de azeite. Sabor e
+              simplicidade!
+            </p>
+            <Botao>Adicionar ao carrinho</Botao>
+          </ProdutoBox>
+        </Produto>
+        <Produto>
+          <ProdutoBox>
+            <ImagemPefil src={imagemPerfil} alt="Pizza" />
+            <h3>Pizza Marguerita</h3>
+            <p>
+              A clássica Marguerita: molho de tomate suculento, mussarela
+              derretida, manjericão fresco e um toque de azeite. Sabor e
+              simplicidade!
+            </p>
+            <Botao>Adicionar ao carrinho</Botao>
+          </ProdutoBox>
+        </Produto>
+      </ProdutoUl>
+    </ProdutoSection>
     <Footer />
   </>
 )
--- src/components/Profile/style.ts
+++ src/components/Profile/style.ts
@@ -4,12 +4,12 @@ import backgroundPerfil from '../../assets/images/backgroundPerfil.png'
 import { cores } from '../../styles'
 
 export const Container = styled.header`
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
+  background-image: url(${backgroundImage});
   height: 186px;
   width: 100%;
-  background-image: url(${backgroundImage});
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
 export const ContainerSmaller = styled.div`
   max-width: 1024px;
@@ -17,8 +17,6 @@ export const ContainerSmaller = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  height: 186px;
-  margin: 0 auto;
   padding: 0 24px;
 `
 export const ContainerImg = styled.div`
@@ -28,8 +26,9 @@ export const ContainerImg = styled.div`
 `
 
 export const Imagem = styled.img`
-  max-width: 125px;
-  max-height: 57px;
+  width: 125px;
+  height: auto;
+  object-fit: contain;
 `
 export const Titles = styled.h2`
   font-size: 18px;
@@ -37,11 +36,11 @@ export const Titles = styled.h2`
 `
 export const Categoria = styled.h4`
   font-size: 32px;
-  font-weight: 400;
+  font-weight: 100;
   color: ${cores.branca};
   margin-bottom: 156px;
 `
-export const NomeRestaurante = styled.h3`
+export const NomeRestaurante = styled.h1`
   font-size: 32px;
   font-weight: bold;
   padding-bottom: 32px;
@@ -50,11 +49,11 @@ export const NomeRestaurante = styled.h3`
 
 export const SubHeader = styled.div`
   background-image: url(${backgroundPerfil});
+  position: relative;
   width: 100%;
   height: 280px;
   background-size: cover;
   background-position: center;
-  position: relative;
 `
 
 export const ContentWrapper = styled.div`
@@ -66,4 +65,64 @@ export const ContentWrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: flex-end;
+`
+export const ImagemPefil = styled.img`
+  max-width: 304px;
+`
+
+export const ProdutoUl = styled.ul`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 32px;
+  margin: 56px 0;
+  max-width: 1024px;
+  margin: 32px auto;
+  width: 100%;
+`
+
+export const ProdutoBox = styled.div`
+  background-color: ${cores.salmao};
+  color: ${cores.SalmaoClaro};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 8px;
+  height: 100%;
+  text-align: left;
+
+  h3 {
+    font-weight: 900;
+    font-size: 16px;
+  }
+  h3,
+  p {
+    width: 100%;
+    text-align: left;
+    margin: 8px 0px;
+  }
+
+  p {
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 22px;
+  }
+
+  button {
+    width: 100%;
+    font-weight: 700;
+    font-size: 14px;
+    align-self: center;
+    background-color: ${cores.SalmaoClaro};
+    color: ${cores.salmao};
+  }
+`
+export const ProdutoSection = styled.section`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  padding-bottom: 120px;
+`
+export const Produto = styled.li`
+  display: flex;
+  height: 100%;
 `
